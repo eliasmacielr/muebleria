acc92840e798dbdd7ffd7f3931a626a402518615
Elias Maciel <eliasmacielr@gmail.com>
2017-01-13 16:32:56 -0300

    Some improvements in product-list

---
+++ webroot/app/product-edit/product-edit.module.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// Define the `productSave` module
+angular.module('productSave', ['core.product', 'core.category']);
--- webroot/app/product-list/product-list.component.js
+++ webroot/app/product-list/product-list.component.js
@@ -6,8 +6,8 @@ angular.
   module('productList').
   component('productList', {
     templateUrl: 'app/product-list/product-list.template.html',
-    controller: ['$routeParams', '$location', '$scope', '$mdDialog', 'Product', 'Category',
-      function ProductListController($routeParams, $location, $scope, $mdDialog, Product, Category) {
+    controller: ['$routeParams', '$location', '$scope', '$mdDialog', '$mdToast', 'Product', 'Category',
+      function ProductListController($routeParams, $location, $scope, $mdDialog, $mdToast, Product, Category) {
 
         var self = this;
 
@@ -33,10 +33,6 @@ angular.
         // }
 
         // Add
-        // this.addProduct = function() {
-        //   Product.add(this.product);
-        // }
-
         $scope.saveProductDialog = function(ev) {
           $mdDialog.show({
             controller: ProductSaveController,
@@ -47,15 +43,27 @@ angular.
           })
           .then(function(answer) {
             // $scope.status = 'You said the information was "' + answer + '".';
+            //self.messageToast(answer);
           }, function() {
             // $scope.status = 'You cancelled the dialog.';
           });
         };
 
         // Edit
-        // this.editProduct = function(editedProduct) {
-        //   Product.edit(editedProduct);
-        // }
+        $scope.editProductDialog = function(ev) {
+          $mdDialog.show({
+            controller: ProductEditController,
+            templateUrl: 'app/product-edit/product-edit.template.html',
+            parent: angular.element(document.body),
+            targetEvent: ev //,
+            // clickOutsideToClose: true
+          })
+          .then(function(answer) { // hide
+            // $scope.status = 'You said the information was "' + answer + '".';
+          }, function() {          // cancel
+            // $scope.status = 'You cancelled the dialog.';
+          });
+        };
 
         // Delete
         self.deleteProduct = function(productId) {
@@ -79,29 +87,67 @@ angular.
 
         this.limitOptions = [5, 10, 15];
 
+        // Controllers
         function ProductSaveController($scope, $mdDialog, Product, Category) {
-          //var self = this;
-
           $scope.categories = Category.list();
 
           $scope.product = {
             /* name
                price
                stock
-               main_image
-               in_offert
-               category_id
-               discount */
+               main_image */
+               in_offert: false,
+               discount: 0
+            /* category_id */
+          };
+
+          $scope.saveProduct = function() {
+            Product.add($scope.product);
+            $scope.answer(''/*response.message*/);
+            // if status is true then push new object in self.products
+          };
+
+          $scope.hide = function() {
+            $mdDialog.hide();
           };
 
           $scope.cancel = function() {
             $mdDialog.cancel();
           };
 
-          $scope.saveProduct = function() {
-            Product.add($scope.product);
+          $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
           };
         }
+
+        function ProductEditController($scope, $mdDialog, Product, Category) {
+          $scope.categories = Category.list();
+
+          $scope.product;
+
+          $scope.cancel = function() {
+            $mdDialog.cancel();
+          };
+
+          $scope.editProduct = function() {
+            Product.edit(
+              {productId: $scope.product.id},
+              $scope.product
+            );
+          };
+        }
+
+        // show toast
+        self.messageToast = function(message) {
+          // var pinTo = '';
+
+          $mdToast.show(
+            $mdToast.simple()
+              .position('bottom left')
+              .textContent(message)
+              .hideDelay(3000)
+          );
+        };
 
       }
     ]
