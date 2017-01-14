d31c60d7fb79f23145241a33b807b2e92e2d9bfa
Elias Maciel <eliasmacielr@gmail.com>
2017-01-14 11:25:44 -0300

    Change customers for users

--- webroot/app/core/customer/customer.module.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-// Define the `core.customer` module
-angular.module('core.customer', ['ngResource']);
---
+++ webroot/app/core/users/user.service.js
@@ -0,0 +1,50 @@
+'use strict';
+
+angular.
+  module('core.user').
+  factory('User', ['$resource',
+    function($resource) {
+
+      this.headers = {
+        'Content-Type' : 'application/json',
+        'Accept' : 'application/json'
+      };
+
+      /*
+      Default actions
+      { 'get':    {method:'GET'},
+        'save':   {method:'POST'},
+        'query':  {method:'GET', isArray:true},
+        'remove': {method:'DELETE'},
+        'delete': {method:'DELETE'} };
+      */
+
+      return $resource('http://localhost/muebleria/api/users/:userId/',
+        {'userId': '@id'},
+        {
+          list: {
+            method: 'GET',
+            headers: this.headers,
+            isArray: false // returns object containing an array called customers
+          },
+          view: {
+            method: 'GET',
+            headers: this.headers
+          },
+          add: {
+            method: 'POST',
+            headers: this.headers
+          },
+          edit: {
+            method: 'PUT',
+            headers: this.headers
+          },
+          delete: {
+            method: 'DELETE',
+            headers: this.headers
+          }
+        }
+      );
+
+    }
+  ]);
