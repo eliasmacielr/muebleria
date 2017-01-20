4468e6af0c25b6bff039eb37fd3ed7820f562016
Elias Maciel <eliasmacielr@gmail.com>
2017-01-20 18:59:16 -0300

    Some improvements

--- webroot/app/category-list/category-list.component.js
+++ webroot/app/category-list/category-list.component.js
@@ -5,8 +5,8 @@ angular.
   module('categoryList').
   component('categoryList', {
     templateUrl: 'app/category-list/category-list.template.html',
-    controller: ['$scope', 'Category',
-      function CategoryListController($scope, Category) {
+    controller: ['$scope', '$mdDialog', '$mdSidenav', '$mdToast', 'Category',
+      function CategoryListController($scope, $mdDialog, $mdSidenav, $mdToast, Category) {
 
         var self = this;
 
@@ -30,12 +30,150 @@ angular.
           this.categories = Category.list();
         };
 
-        $scope.toggleRight = buildToggler('right');
+        // Add
+        $scope.categoryAddDialog = function(ev) {
+          $mdDialog.show({
+            controller: CategoryAddController,
+            templateUrl: 'app/category-add/category-add.template.html',
+            parent: angular.element(document.body),
+            targetEvent: ev
+          })
+          .then(function(answer) { // hide
+            self.messageToast(answer);
+          }, function() { // cancel
+          });
+        };
 
-        function buildToggler(componentId) {
-          return function() {
-            $mdSidenav(componentId).toggle();
+        // Edit
+        $scope.categoryEditDialog = function(ev, category) {
+          self.category = category;
+          $mdDialog.show({
+            controller: CategoryEditController,
+            templateUrl: 'app/category-edit/category-edit.template.html',
+            parent: angular.element(document.body),
+            targetEvent: ev
+          })
+          .then(function(answer) { // hide
+            self.messageToast(answer);
+          }, function() { // cancel
+            self.messageToast('No se ha editado el registro');
+          });
+        };
+
+        // Delete
+        $scope.categoryDeleteDialog = function(ev) {
+          var confirm = $mdDialog.confirm()
+            .title('Eliminar categorías')
+            .textContent('Está seguro de borrar ' + self.selected.length + ' registro(s)?')
+            .ariaLabel('Eliminar categorías')
+            .targetEvent(ev)
+            .ok('Aceptar')
+            .cancel('Cancelar');
+
+          $mdDialog.show(confirm).then(function() {
+            self.deleteCategories();
+          }, function() {
+            self.messageToast('No se borraron registros');
+          });
+        };
+
+        // Controllers
+        function CategoryAddController($scope, $mdDialog, Category) {
+          $scope.category = {};
+
+          $scope.addCategory = function() {
+            Category.add($scope.category,
+              function (response) {
+                if (response.status) {
+                  self.categories.categories.push(response.category);
+                  self.categories.pagination.count += 1;
+                }
+                $scope.answer(response.message);
+              }
+            );
+          };
+
+          $scope.hide = function() {
+            $mdDialog.hide();
+          };
+
+          $scope.cancel = function() {
+            $mdDialog.cancel();
+          };
+
+          $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+          };
+        };
+
+        function CategoryEditController($scope, $mdDialog, Category) {
+          $scope.toEdit = Category.view({categoryId: self.category.id});
+
+          var index = self.categories.categories.indexOf(self.category);
+
+          $scope.editCategory = function() {
+            Category.edit(
+              {categoryId: $scope.toEdit.category.id},
+              $scope.toEdit.category,
+              function (response) {
+                if (response.status) {
+                  self.categories.categories[index] = response.category; // replace edited element
+                }
+                $scope.answer(response.message);
+              }
+            );
+          };
+
+          $scope.hide = function() {
+            $mdDialog.hide();
+          };
+
+          $scope.cancel = function() {
+            $mdDialog.cancel();
+          };
+
+          $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+          };
+        };
+
+        self.deleteCategory = function(categoryId) {
+          Category.delete({categoryId: categoryId});
+        };
+
+        self.deleteCategories = function() {
+          var allDeleted = true;
+          for(var i = 0; i < self.selected.length; i++) {
+            self.deleteCategory(
+              self.selected[i].id,
+              function(response) {
+                allDeleted *= response.status;
+              }
+            );
+            var index = self.categories.categories.indexOf(self.selected[i]);
+            self.categories.categories.splice(index, 1); // delete one element
           }
+
+          self.categories.pagination.count -= self.selected.length; // update count
+          self.selected = []; // empty self.selected array
+
+          if (allDeleted) {
+            self.messageToast('Se han borrado los registros');
+          }
+        };
+
+        self.messageToast = function(message) {
+          $mdToast.show(
+            $mdToast.simple()
+              .position('bottom left')
+              .textContent(message)
+              .hideDelay(3000)
+          );
+        };
+
+        // Navigation sidenav
+        $scope.showLeftSidenav = function() {
+          $mdSidenav('left').toggle();
         };
 
       }
