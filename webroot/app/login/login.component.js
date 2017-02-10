7058a22d0714d8b8afbcddb22d539c534c6b626e
Elias Maciel <eliasmacielr@gmail.com>
2017-02-10 13:17:48 -0300

    Add functionality for login and improve the UI

--- webroot/app/login/login.component.js
+++ webroot/app/login/login.component.js
@@ -5,6 +5,28 @@ angular.
   module('login').
   component('login', {
     templateUrl: 'app/login/login.template.html',
-    controller: function LoginController() {
-    }
+    controller: ['Auth',
+      function LoginController(Auth) {
+
+        var self = this;
+
+        Auth.clearCredentials();
+
+        self.login = function () {
+          Auth.login(self.username, self.password,
+            function (response) {
+              // go to dashboard
+              if (response.status) {
+                console.log('You are now logged in');
+              }
+            }
+          );
+        };
+
+        self.logout = function () {
+          Auth.logout();
+        };
+
+      }
+    ]
   });
