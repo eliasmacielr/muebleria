5e3cbd6c2e60c32971271ef9ab8b6b59a373ca07
Elias Maciel <eliasmacielr@gmail.com>
2017-02-11 15:46:09 -0300

    Improve view and logic

--- webroot/app/login/login.component.js
+++ webroot/app/login/login.component.js
@@ -5,26 +5,33 @@ angular.
   module('login').
   component('login', {
     templateUrl: 'app/login/login.template.html',
-    controller: ['Auth',
-      function LoginController(Auth) {
+    controller: ['$http', '$location', 'Auth',
+      function LoginController($http, $location, Auth) {
 
         var self = this;
+
+        self.user = {};
 
         Auth.clearCredentials();
 
         self.login = function () {
-          Auth.login(self.username, self.password,
+          Auth.login(self.user.username, self.user.password,
             function (response) {
-              // go to dashboard
               if (response.status) {
-                console.log('You are now logged in');
+                $location.path('/bandeja');
               }
             }
           );
         };
 
         self.logout = function () {
-          Auth.logout();
+          Auth.logout(
+            function (response) {
+              if (response.status) {
+                $location.path('/login');
+              }
+            }
+          );
         };
 
       }
