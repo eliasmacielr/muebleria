84eb8c6c71c5e8042947ac0edc095c8345a54dda
Elias Maciel <eliasmacielr@gmail.com>
2017-02-11 16:32:30 -0300

    Add logout button

--- webroot/app/dashboard/dashboard.component.js
+++ webroot/app/dashboard/dashboard.component.js
@@ -5,10 +5,14 @@ angular.
   module('dashboard').
   component('dashboard', {
     templateUrl: 'app/dashboard/dashboard.template.html',
-    controller: ['$scope', '$mdSidenav',
-      function DashboardController($scope, $mdSidenav) {
+    controller: ['$scope', '$mdSidenav', 'Auth',
+      function DashboardController($scope, $mdSidenav, Auth) {
 
         var self = this;
+
+        self.logout = function () {
+          Auth.logout();
+        };
 
         $scope.showLeftSidenav = function () {
           $mdSidenav('left').toggle();
