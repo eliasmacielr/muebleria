096898311d75489b4b1a8f06e5b65d10bf026d4f
Sergio Alcaraz <siac215@gmail.com>
2017-02-15 20:13:11 -0300

    Add ContactForm for modelless form

---
+++ src/Form/ContactForm.php
@@ -0,0 +1,59 @@
+<?php
+namespace App\Form;
+
+use Cake\Form\Form;
+use Cake\Form\Schema;
+use Cake\Mailer\Email;
+use Cake\Validation\Validator;
+
+/**
+ * Contact Form.
+ */
+class ContactForm extends Form
+{
+    /**
+     * Builds the schema for the modelless form
+     *
+     * @param \Cake\Form\Schema $schema From schema
+     * @return \Cake\Form\Schema
+     */
+    protected function _buildSchema(Schema $schema)
+    {
+        return $schema->addField('name', ['type' => 'string'])
+            ->addField('email', ['type' => 'email'])
+            ->addField('phone', ['type' => 'string'])
+            ->addField('message', ['type' => 'string']);
+    }
+
+    /**
+     * Form validation builder
+     *
+     * @param \Cake\Validation\Validator $validator to use against the form
+     * @return \Cake\Validation\Validator
+     */
+    protected function _buildValidator(Validator $validator)
+    {
+        return $validator
+            ->notEmpty('name')
+            ->notEmpty('email')
+            ->email('email')
+            ->notEmpty('phone')
+            ->notEmpty('message');
+    }
+
+    /**
+     * Defines what to execute once the From is being processed
+     *
+     * @param array $data Form data.
+     * @return bool
+     */
+    protected function _execute(array $contact)
+    {
+        $email = new Email('default');
+        $email->emailFormat('html')->template('contact')->viewVars($contact)
+            ->from([$contact['email'] => $contact['name']])
+            ->subject($contact['title'])
+            ->send();
+        return true;
+    }
+}
