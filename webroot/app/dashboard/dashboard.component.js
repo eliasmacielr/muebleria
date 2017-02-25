83f1115b86db024b3cea18a2de2cd5bb542e4ef2
Elias Maciel <eliasmacielr@gmail.com>
2017-02-25 00:49:31 -0300

    Add some data in the dashboard

--- webroot/app/dashboard/dashboard.component.js
+++ webroot/app/dashboard/dashboard.component.js
@@ -5,10 +5,35 @@ angular.
   module('dashboard').
   component('dashboard', {
     templateUrl: 'app/dashboard/dashboard.template.html',
-    controller: ['$http', '$location', '$scope', '$mdSidenav',
-      function DashboardController($http, $location, $scope, $mdSidenav) {
+    controller: ['$http', '$location', '$scope', '$mdSidenav', 'Product',
+      function DashboardController($http, $location, $scope, $mdSidenav, Product) {
 
         var self = this;
+
+        // TOTAL
+        Product.list().$promise.then(
+          function (response) {
+            if (response.status) {
+              self.totalProducts = response.pagination.count;
+            }
+          }
+        );
+        // Available
+        Product.list({available: 1}).$promise.then(
+          function (response) {
+            if (response.status) {
+              self.availableProducts = response.pagination.count;
+            }
+          }
+        );
+        // In offer
+        Product.list({available: 1, in_offer: 1}).$promise.then(
+          function (response) {
+            if (response.status) {
+              self.inOfferProducts = response.pagination.count;
+            }
+          }
+        );
 
         $scope.showLeftSidenav = function () {
           $mdSidenav('left').toggle();
